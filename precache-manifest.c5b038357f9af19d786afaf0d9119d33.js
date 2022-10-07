69ecaabe2f3dbfa2d1a8d494d73d27f1c5079584
Olayinka <ademayowa4@gmail.com>
2022-10-07 10:54:20 +0800

    Updates

---
+++ precache-manifest.c5b038357f9af19d786afaf0d9119d33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f714c7113157d90e0384",
+    "url": "/React-calc/static/css/main.b7ffbe8c.chunk.css"
+  },
+  {
+    "revision": "f714c7113157d90e0384",
+    "url": "/React-calc/static/js/main.9130e653.chunk.js"
+  },
+  {
+    "revision": "7cefb0e7788fbd457436",
+    "url": "/React-calc/static/js/runtime~main.fb68496c.js"
+  },
+  {
+    "revision": "ec0e28bb755c6ae50b26",
+    "url": "/React-calc/static/js/2.3edbe4ce.chunk.js"
+  },
+  {
+    "revision": "97e715952a826070d1f81bd93a30127c",
+    "url": "/React-calc/index.html"
+  }
+];
